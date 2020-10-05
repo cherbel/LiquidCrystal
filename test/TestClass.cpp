6d06d4caf022f8fd3dcab54c9cbf761e97b14ea0
caherbel <caherbel@gmail.com>
2020-10-05 10:19:12 -0600

    WIP

--- test/TestClass.cpp
+++ test/TestClass.cpp
@@ -52,4 +52,64 @@ unittest(clearBuffer) {
   assertEqual(0, lines.at(1).length());
 }
 
+unittest(print_high) {
+
+  //   size_t print(const String &s)                 { return write(s.c_str(), s.length()); }
+  //   size_t print(const char* str)                 { return print(String(str)); }
+  //   size_t print(char c)                          { return print(String(c)); }
+  //   size_t print(unsigned char b, int base = DEC) { return print(String(b, base)); }
+  //   size_t print(int n,           int base = DEC) { return print(String(n, base)); }
+
+  std::vector<std::string> lines;
+  // create lcd object
+  LiquidCrystal_Test lcd(rs, enable, d4, d5, d6, d7);
+  // reset lcd to have two lines
+  lcd.begin(16, 2);
+  // write something to display
+  lcd.print("C String");
+  lines = lcd.getLines();
+  assertEqual(2, lines.size());
+  assertEqual(11, lines.at(0).length());
+  assertEqual("C String", lines.at(0));
+  assertEqual(0, lines.at(1).length());
+  lcd.clear();
+
+  
+  lcd.print(String("Test String"));
+  lines = lcd.getLines();
+  assertEqual(2, lines.size());
+  assertEqual(11, lines.at(0).length());
+  assertEqual("Test String", lines.at(0));
+  assertEqual(0, lines.at(1).length());
+  lcd.clear();
+
+  unsigned char testUnsignedChar = "
+  lcd.print(testUnsignedChar, DEC);
+  lines = lcd.getLines();
+  assertEqual(2, lines.size());
+  assertEqual(11, lines.at(0).length());
+  assertEqual("Test String", lines.at(0));
+  assertEqual(0, lines.at(1).length());
+  lcd.clear();
+
+  lcd.print(String("Test String"));
+  lines = lcd.getLines();
+  assertEqual(2, lines.size());
+  assertEqual(11, lines.at(0).length());
+  assertEqual("Test String", lines.at(0));
+  assertEqual(0, lines.at(1).length());
+  lcd.clear();
+
+  lcd.print(String("Test String"));
+  lines = lcd.getLines();
+  assertEqual(2, lines.size());
+  assertEqual(11, lines.at(0).length());
+  assertEqual("Test String", lines.at(0));
+  assertEqual(0, lines.at(1).length());
+  lcd.clear();
+
+}
+
+
+
 unittest_main()
